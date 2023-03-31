ec64f7fa1e2ba021f32bbe08a80fc6dec9ebaab9
Oliga2505 <114357615+Oliga2505@users.noreply.github.com>
2023-03-31 00:06:19 -0400

    Reverted cukes runner to original state

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
-        dryRun = false,
-        tags = "@wip"
+        dryRun = true,
+        tags = ""
 )
 public class CukesRunner {
 
